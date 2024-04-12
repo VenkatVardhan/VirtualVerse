eaa604c759eec222466e533552b03fe8ea179e09
VenkatVardhan <saivenkat1579@gmail.com>
2024-04-12 16:58:14 +0530

    Created Sign In api Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
+
+
 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
@@ -27,4 +30,40 @@ export const signup = async (req, res,next) => {
   } catch (error) {
     next(error);
   }
+}
+export const signin = async(req,res,next)=>{
+
+  const {email,password}=req.body;
+  if(!email|| !password ||email ==='' || password ===''){
+    return next(errorHandler(500,'All Fields are Required'))
+  }
+  try{
+    const validUser= await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404,'User not found'));
+      
+    }
+    const valid =bcryptjs.compareSync(password,validUser.password);
+    if(!valid){
+      return next(errorHandler(400,'Invalid Password '));
+      
+
+    }
+    const {password:pass,...rest}=validUser._doc;
+    const token =  jwt.sign({
+      id:validUser._id},process.env.JWT_SECRETKEY);
+    res.status(200).cookie('acessToken',token,{
+      httpOnly:true}).json(rest);
+  
+
+  }
+  catch(error){
+    next(error);
+
+  }
+
+
+
+
+
 }
